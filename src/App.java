55a868103ca6261f2b841360ec910dd403195ae5
jericho909 <omerahmetyucel@gmail.com>
2024-01-22 17:27:29 +0300

    edits

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+import business.UserManager;
 import core.Helper;
+import view.AdminView;
 import view.LoginView;
 
 public class App {
     public static void main(String[] args) {
         Helper.setTheme();
         LoginView loginView = new LoginView();
+        //UserManager userManager = new UserManager();
+        //AdminView adminView = new AdminView(userManager.fetchUserWithLoginInfo("admin", "1234"));
     }
 }

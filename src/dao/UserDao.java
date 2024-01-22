393b89c6769f71eb523cd4f49e4e1243bbee55c2
jericho909 <omerahmetyucel@gmail.com>
2024-01-22 16:23:56 +0300

    created dao package; which will eventually contain all the database operations for all the entitites

---
+++ src/dao/UserDao.java
@@ -0,0 +1,69 @@
+package dao;
+
+import core.DbConnection;
+import entities.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserDao {
+
+    public final Connection connection;
+
+
+    public UserDao() {
+        this.connection = DbConnection.getInstance();
+    }
+
+    public ArrayList<User> fetchAllUsers(){
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        try {
+            String query = "SELECT * FROM public.user";
+            ResultSet rs = this.connection.createStatement().executeQuery(query);
+
+            while (rs.next()){
+                userArrayList.add(convertDatabaseValueToClass(rs));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return userArrayList;
+    }
+
+    public User fetchUserWithLoginInfo(String username, String password){
+        User user = null;
+
+        String query = "SELECT * FROM public.user WHERE user_name = ? AND user_password = ?";
+
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()){
+                user = this.convertDatabaseValueToClass(rs);
+            }
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return user;
+    }
+
+    public User convertDatabaseValueToClass(ResultSet rs) throws SQLException {
+        User user = new User();
+        user.setId(rs.getInt("id"));
+        user.setUser_name(rs.getString("user_name"));
+        user.setUser_password(rs.getString("user_password"));
+        user.setUser_role(rs.getString("user_role"));
+
+        return user;
+    }
+}

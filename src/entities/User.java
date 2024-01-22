734dd10b087ed03a8697772e212b547a4ca9d996
jericho909 <omerahmetyucel@gmail.com>
2024-01-22 14:40:38 +0300

    created entites package, just has Users class now

---
+++ src/entities/User.java
@@ -0,0 +1,58 @@
+package entities;
+
+public class User {
+
+    private int id;
+    private String user_name;
+    private String user_password;
+    private String user_role;
+
+    public User(int id, String user_name, String user_password, String user_role) {
+        this.id = id;
+        this.user_name = user_name;
+        this.user_password = user_password;
+        this.user_role = user_role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getUser_role() {
+        return user_role;
+    }
+
+    public void setUser_role(String user_role) {
+        this.user_role = user_role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", user_name='" + user_name + '\'' +
+                ", user_password='" + user_password + '\'' +
+                ", user_role='" + user_role + '\'' +
+                '}';
+    }
+}

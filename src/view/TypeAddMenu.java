708877831e10b705d4a04c77eb72f122012e5b88
jericho909 <omerahmetyucel@gmail.com>
2024-01-26 02:35:54 +0300

    added functionality to the save button

--- src/view/TypeAddMenu.java
+++ src/view/TypeAddMenu.java
@@ -3,6 +3,7 @@ package view;
 import business.HotelManager;
 import business.TypeManager;
 import core.ComboItem;
+import core.Helper;
 import entities.Hotel;
 
 import javax.swing.*;
@@ -15,10 +16,12 @@ public class TypeAddMenu extends Layout {
     private JLabel lbl_hoteltypename;
     private JButton btn_save;
     private JPanel container;
+    private JComboBox cmb_types;
     private TypeManager typeManager;
     private HotelManager hotelManager;
 
     public TypeAddMenu() {
+
         this.typeManager = new TypeManager();
         this.hotelManager = new HotelManager();
         this.add(container);
@@ -28,5 +31,12 @@ public class TypeAddMenu extends Layout {
         for (Hotel hotel: this.hotelManager.fetchAllHotels()){
             this.cmb_hotelnames.addItem(new ComboItem(hotel.getId(), hotel.getHotel_name()));
         }
+        btn_save.addActionListener(e -> {
+            boolean result = false;
+            ComboItem selectedHotel = (ComboItem) cmb_hotelnames.getSelectedItem();
+            result = this.typeManager.saveType(new entities.Type(selectedHotel.getKey(), cmb_types.getSelectedItem().toString()));
+            Helper.showCustomMessage("Added new type.", "Operation successful.");
+            dispose();
+        });
     }
 }

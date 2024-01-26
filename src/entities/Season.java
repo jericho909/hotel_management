6088e6b5365e826f4d45862f7a4e648566ca89a6
jericho909 <omerahmetyucel@gmail.com>
2024-01-26 14:47:09 +0300

    added new properties to season class

--- src/entities/Season.java
+++ src/entities/Season.java
@@ -3,15 +3,19 @@ package entities;
 import java.time.LocalDate;
 
 public class Season {
-    int id;
-    int hotel_id;
-    LocalDate season_start_date;
-    LocalDate season_end_date;
+    private int id;
+    private int hotel_id;
+    private LocalDate season_start_date;
+    private LocalDate season_end_date;
+    private String season_name;
+    private Double season_rate;
 
-    public Season(int hotel_id, LocalDate season_start_date, LocalDate season_end_date) {
+    public Season(int hotel_id, LocalDate season_start_date, LocalDate season_end_date, String season_name, Double season_rate) {
         this.hotel_id = hotel_id;
         this.season_start_date = season_start_date;
         this.season_end_date = season_end_date;
+        this.season_name = season_name;
+        this.season_rate = season_rate;
     }
 
     public Season() {
@@ -49,6 +53,22 @@ public class Season {
         this.season_end_date = season_end_date;
     }
 
+    public String getSeason_name() {
+        return season_name;
+    }
+
+    public void setSeason_name(String season_name) {
+        this.season_name = season_name;
+    }
+
+    public Double getSeason_rate() {
+        return season_rate;
+    }
+
+    public void setSeason_rate(Double season_rate) {
+        this.season_rate = season_rate;
+    }
+
     @Override
     public String toString() {
         return "Season{" +
@@ -56,6 +76,8 @@ public class Season {
                 ", hotel_id=" + hotel_id +
                 ", season_start_date=" + season_start_date +
                 ", season_end_date=" + season_end_date +
+                ", season_name='" + season_name + '\'' +
+                ", season_rate=" + season_rate +
                 '}';
     }
 }

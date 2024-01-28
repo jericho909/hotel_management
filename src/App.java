828a0c6c3ab43ca624496bb9e3e8df6b46c38bdc
jericho909 <omerahmetyucel@gmail.com>
2024-01-28 17:20:41 +0300

    removed unnecessary comment lines

--- src/App.java
+++ src/App.java
@@ -7,7 +7,5 @@ public class App {
     public static void main(String[] args) {
         Helper.setTheme();
         LoginView loginView = new LoginView();
-        //UserManager userManager = new UserManager();
-        //AdminView adminView = new AdminView(userManager.fetchUserWithLoginInfo("admin", "1234"));
     }
 }

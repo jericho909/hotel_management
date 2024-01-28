e6c2053435263560de031437a2b50c1d8b075965
jericho909 <omerahmetyucel@gmail.com>
2024-01-29 01:43:35 +0300

    added functions for editing existing reservations

--- src/dao/ReservationDao.java
+++ src/dao/ReservationDao.java
@@ -39,7 +39,8 @@ public class ReservationDao {
         reservation.setRoom_id(rs.getInt("room_id"));
         reservation.setReservation_start_date(LocalDate.parse(rs.getString("reservation_str_date")));
         reservation.setReservation_end_date(LocalDate.parse(rs.getString("reservation_end_date")));
-        reservation.setReservation_guest_name(rs.getString("reservation_guest_phone"));
+        reservation.setReservation_guest_name(rs.getString("reservation_guest_name"));
+        reservation.setReservation_guest_phone(rs.getString("reservation_guest_phone"));
         reservation.setReservation_total_price(rs.getDouble("reservation_total_price"));
 
         return reservation;
@@ -81,6 +82,26 @@ public class ReservationDao {
             return preparedStatement.executeUpdate() != -1;
 
         }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public boolean editReservation(Reservation reservation){
+        String query = "UPDATE public.reservation SET room_id = ?, reservation_str_date = ?, reservation_end_date = ?, " +
+                "reservation_guest_name = ?, reservation_guest_phone = ?, reservation_total_price = ? WHERE id = ?";
+
+        try {
+            PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+            preparedStatement.setInt(1, reservation.getRoom_id());
+            preparedStatement.setDate(2, Date.valueOf(reservation.getReservation_start_date()));
+            preparedStatement.setDate(3, Date.valueOf(reservation.getReservation_end_date()));
+            preparedStatement.setString(4, reservation.getReservation_guest_name());
+            preparedStatement.setString(5,reservation.getReservation_guest_phone());
+            preparedStatement.setDouble(6, reservation.getReservation_total_price());
+            preparedStatement.setInt(7, reservation.getId());
+            return preparedStatement.executeUpdate() != 1;
+        } catch (SQLException e){
             e.printStackTrace();
         }
         return true;
